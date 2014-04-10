b3ca563b75dfd2c4ba48844ebab892ce561184a4
ABaptista <contact@andrebaptista.com>
2014-04-10 18:43:40 +0100

    Weapon selection added.

--- client/views/join.js
+++ client/views/join.js
@@ -1,7 +1,43 @@
+language = "javascript";
+
 Template.join.events({
   'click .submit-button': function(event) {
     event.preventDefault();
 
     Meteor.call("preStart");
+  },
+
+  'click #ruby' : function(event) {
+    selectRuby();
+  },
+
+  'click #javascript' : function(event) {
+    selectJavascript();
+  },
+
+  'click #python' : function(event) {
+    selectPython();
   }
 });
+
+selectRuby = function() {
+  language = "ruby";
+  $("#ruby").addClass("selected");
+  $("#javascript").removeClass("selected");
+  $("#python").removeClass("selected");
+}
+
+selectJavascript = function() {
+  language = "javascript";
+  $("#ruby").removeClass("selected");
+  $("#javascript").addClass("selected");
+  $("#python").removeClass("selected");
+}
+
+selectPython = function() {
+  language = "python";
+  $("#ruby").removeClass("selected");
+  $("#javascript").removeClass("selected");
+  $("#python").addClass("selected");
+}
+
