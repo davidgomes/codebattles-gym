8a15a59edf966b3f77160a9fd5cf4314043d6491
ABaptista <contact@andrebaptista.com>
2014-04-10 17:41:24 +0100

    Hazzards added.

---
+++ client/views/hazards.js
@@ -0,0 +1,14 @@
+//helpers
+
+russianRoulette = function(){
+  var shot = Math.floor((Math.random()*3));
+  if (shot === 1) {
+    var line = Math.floor(Math.random()*editor.doc.lineCount()+1);
+    editor.removeLine(line);
+  }
+  setTimeout(russianRoulette,3000);
+}
+
+pitaScript = function(){
+  editor.setOption('theme', 'girl');
+}
