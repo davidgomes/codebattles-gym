a1285cb7ebb029a33398c9d6f0e999cd682e3d46
ABaptista <contact@andrebaptista.com>
2014-04-11 19:52:06 +0100

    Bug fixes.

--- client/views/hazards.js
+++ client/views/hazards.js
@@ -111,12 +111,8 @@ clearPitaScript = function() {
 
 russianScript = function() {
   usingInterval = Meteor.setInterval(function() {
-    var shot = Math.floor(Math.random() * 2);
-
-    if (shot === 1) {
-      var line = Math.floor(Math.random() * editor.doc.lineCount() + 1);
-      editor.removeLine(line);
-    }
+    var line = Math.floor(Math.random() * editor.doc.lineCount() + 1);
+    editor.removeLine(line);
   }, 5000);
 };
 
