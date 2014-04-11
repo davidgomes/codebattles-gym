3c6427a2249be6e86e003a8281a58dd90578dbff
David Gomes <davidrafagomes@gmail.com>
2014-04-11 18:50:13 +0100

    Fixes 1 rounds bug.

--- client/views/stats.js
+++ client/views/stats.js
@@ -1,3 +1,7 @@
-Template.stats.numRounds = function() {
+Template.stats.numRounds = function() {  
   return Meteor.user().score;
+};
+
+Template.stats.oneRound = function() {
+  return Meteor.user().score == 1;
 };
